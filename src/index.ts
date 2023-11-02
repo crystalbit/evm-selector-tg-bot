8807f703f925f2b234b7e77b484bf1013ce4d463
memexpo <dmitry@marscolony.io>
2023-11-02 15:40:16 +0300

    logs

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ import { findMethods } from './find-methods';
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.on(message('text'), async (ctx) => {
-  // console.log(ctx.message.text);
+  console.log(ctx.update.message.from.username ?? ctx.update.message.from.id, ctx.message.text.substring(0, 20));
+  if (ctx.message.text === '/start') {
+    await ctx.reply('Hello there :)\nSend me a solidity contract and I will find all methods for you\n\nContribute: https://github.com/crystalbit/evm-selector-tg-bot');
+    return;
+  }
   const result = findMethods(ctx.message.text);
   console.log(result);
   const answer = result.length > 0 ? result.join('\n') : 'No methods found';
